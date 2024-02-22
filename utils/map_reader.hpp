9c6ba57c959aa6355f51a42441c043e72ab8755e
antonio-decaro <antonio.decaro99@outlook.it>
2024-02-22 16:09:13 +0000

    inserte control to disable frequency scaling

--- utils/map_reader.hpp
+++ utils/map_reader.hpp
@@ -1,6 +1,9 @@
 #include <map> 
 #include <string>
 #include <iostream>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/types.h>
 
 
 class FreqManager {
@@ -11,27 +14,27 @@ public:
     KERNEL
   };
 
-  FreqManager(std::istream& is) {
-    try {
-      init(is);
-    } catch (std::exception e) {
-      std::cerr << e.what() << std::endl;
-    }
+  FreqManager(std::istream& is) { 
+    init(is);
   }
 
   FreqManager() = default;
 
   void init(std::istream& is) {
-    read_policy(is);
-    read_map(is);
+    if (isInputAvailable()) {
+      read_policy(is);
+      read_map(is);
+      enabled = true;
+    } else {
+      std::cerr << "Frequency scaling disabled" << std::endl;
+      enabled = false;
+    }
   }
 
   double getAndSetFreq(const std::string& key) {
+    if (!enabled) return 0;
+
     double freq = 0;
-    if (kernel_freqs.find(key) == kernel_freqs.end()) {
-      std::cerr << "Kernel name '" + key + "' not found" << std::endl;
-      return 0;
-    }
     freq = kernel_freqs[key];
     switch (policy) {
       case FreqChangePolicy::APP:
@@ -73,7 +76,7 @@ private:
     std::string key, value, keep;
     while (is >> key >> value >> keep) {
       keep_freq[key] = keep == "KEEP";
-      kernel_freqs[key] = atof(value.c_str());
+      kernel_freqs[key] = std::stod(value);
     }
   }
 
@@ -83,7 +86,25 @@ private:
     this->policy = policyFromString(policy);
   }
 
+  // Function to check if input is available without blocking
+  bool isInputAvailable() {
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    
+    int ret = select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
+    if (ret == -1) {
+      throw std::runtime_error("Error in select()");
+    }
+    return ret > 0;
+  }
+
   std::map<std::string, double> kernel_freqs;
   std::map<std::string, bool> keep_freq;
+  bool enabled;
   FreqChangePolicy policy;
 };
