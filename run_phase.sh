d3dc17988bd67717a385ccbffd334e5765d88463
antonio-decaro <antonio.decaro99@outlook.it>
2024-02-26 18:28:54 +0000

    Update number of rows and columns in SRAD

--- run_phase.sh
+++ run_phase.sh
@@ -118,8 +118,8 @@ for it in $(seq 1 $num_runs); do
   echo "[*] Running SRAD"
   num_iters=1
   lambda=1
-  number_of_rows=16384 #512
-  number_of_cols=16384 #512
+  number_of_rows=8192 #16384 #512
+  number_of_cols=8192 #16384 #512
   cat $CONF_DIR/app/srad.conf | ./srad_main $num_iters $lambda $number_of_rows $number_of_cols > $LOG_DIR/srad/srad_app$it.csv 2> $LOG_DIR/srad/srad_app$it.log
   cat $CONF_DIR/phase/srad.conf | ./srad_main $num_iters $lambda $number_of_rows $number_of_cols > $LOG_DIR/srad/srad_phase$it.csv 2> $LOG_DIR/srad/srad_phase$it.log
   cat $CONF_DIR/kernel/srad.conf | ./srad_main $num_iters $lambda $number_of_rows $number_of_cols > $LOG_DIR/srad/srad_kernel$it.csv 2> $LOG_DIR/srad/srad_kernel$it.log
