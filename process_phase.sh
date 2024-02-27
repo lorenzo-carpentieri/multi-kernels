0a89e65c4c617186ebca1357a06313985c3a05c3
antonio-decaro <antonio.decaro99@outlook.it>
2024-02-27 17:16:32 +0000

    added script for making plots and parsing data

---
+++ process_phase.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+parse_data=false
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --parse)
+      parse_data=true
+      shift
+      ;;
+    *)
+    echo "Invalid argument: $1"
+      return 1 2>/dev/null
+      exit 1
+      ;;
+  esac
+done
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ $parse_data = true ] ; then
+    echo "[*] Parsing data..."
+    python3 $SCRIPT_DIR/scripts/parse_phase_logs.py $SCRIPT_DIR/logs/phase/native $SCRIPT_DIR/parsed/phase/phase_results.csv
+fi
+python3 $SCRIPT_DIR/scripts/plot_phase_results.py kernels $SCRIPT_DIR/parsed/phase/phase_results.csv $SCRIPT_DIR/plot
+python3 $SCRIPT_DIR/scripts/plot_phase_results.py total $SCRIPT_DIR/parsed/phase/phase_results.csv $SCRIPT_DIR/plot
