271f50af2eee621462a7d494c72201ee72c556d2
antonio-decaro <antonio.decaro99@outlook.it>
2024-03-08 20:00:09 +0000

    Add script for finding frequencies on different architectures

---
+++ scripts/find_freq.py
@@ -0,0 +1,229 @@
+#!/bin/env python3
+
+import argparse
+import statistics
+import sys, os, re, subprocess
+import types
+import pandas as pd
+from enum import Enum
+from typing import List
+from io import StringIO
+
+ARCH = None
+RUNS = 1
+
+
+def get_frequencies() -> List[int]:
+  if ARCH == "intel":
+    raise NotImplementedError("Intel frequency not implemented")
+  elif ARCH == "amd":
+    raise NotImplementedError("AMD frequency not implemented")
+  elif ARCH == "nvidia":
+    out = subprocess.run(["nvidia-smi", "--query-supported-clocks=gr", "--format=csv,noheader"], capture_output=True, text=True).stdout.split()
+    out = list(map(lambda x: x.replace("MHz", "").replace(" ", ""), out))
+    out = list(filter(lambda x: x.isnumeric(), out))
+    out.reverse()
+    return list(map(int, out))
+  else:
+    raise ValueError(f"Invalid architecture {ARCH}")
+
+def set_frequency(freq: int):
+  if ARCH == "intel":
+    raise NotImplementedError("Intel frequency not implemented")
+  elif ARCH == "amd":
+    raise NotImplementedError("AMD frequency not implemented")
+  elif ARCH == "nvidia":
+    proc = subprocess.run(["nvidia-smi", "-lgc", f"{freq}"], capture_output=True, text=True)
+    if proc.returncode != 0:
+      raise RuntimeError(f"Failed to set frequency to {freq}")
+  else:
+    raise ValueError(f"Invalid architecture {ARCH}")
+
+def get_default_freq() -> int:
+  if ARCH == "intel":
+    raise NotImplementedError("Intel frequency not implemented")
+  elif ARCH == "amd":
+    raise NotImplementedError("AMD frequency not implemented")
+  elif ARCH == "nvidia":
+    out = subprocess.run(["nvidia-smi", "--query-gpu=clocks.gr", "--format=csv,noheader"], capture_output=True, text=True).stdout
+    return int(out.split()[0])
+  else:
+    raise ValueError(f"Invalid architecture {ARCH}")
+
+def reset_frequency():
+  if ARCH == "intel":
+    raise NotImplementedError("Intel frequency not implemented")
+  elif ARCH == "amd":
+    raise NotImplementedError("AMD frequency not implemented")
+  elif ARCH == "nvidia":
+    subprocess.run(["nvidia-smi", "-rgc"])
+  else:
+    raise ValueError(f"Invalid architecture {ARCH}")
+
+
+class Target(Enum):
+  ME = "ME"
+  MP = "MP"
+  MEDP = "MEDP"
+  
+  def __str__(self):
+    return self.value
+  
+  def from_str(s: str) -> 'Target':
+    if s == "ME":
+      return Target.ME
+    elif s == "MP":
+      return Target.MP
+    elif s == "MEDP":
+      return Target.MEDP
+    else:
+      raise ValueError(f"Invalid target {s}")
+   
+   
+class Result:
+  def __parse_output(self, output: str) -> float:
+    df = pd.read_csv(StringIO(output))
+    self.kernels = {}
+    self.time = df['total_real_time[ms]'].mean()
+    self.energy = df["total_device_energy[j]"].mean()
+    self.kernels_energy = df["sum_kernel_energy[j]"].mean()
+    self.kernels_time = df['sum_kernel_times[ms]'].mean()
+    for n, g in df.groupby('kernel_name'):
+      g: pd.DataFrame
+      self.kernels[n] = types.SimpleNamespace()
+      self.kernels[n].time = g['times[ms]'].sum()
+      self.kernels[n].energy = g['kernel_energy[j]'].sum()
+      self.kernels[n].invocations = len(g)
+      self.kernels[n].time_percentage_total = self.kernels[n].time * 100 / self.time
+      self.kernels[n].time_percentage_device = self.kernels[n].time * 100 / self.kernels_time
+  
+  def __getitem__(self, key):
+    return self.kernels[key]
+  
+  def get_value(self, target: Target, kernel: str = None) -> float:
+    obj = self if kernel is None else self.kernels[kernel]
+    if target == Target.ME:
+      return obj.energy
+    elif target == Target.MP:
+      return obj.time
+    elif target == Target.MEDP:
+      return obj.time * obj.energy
+    else:
+      raise ValueError(f"Invalid target {target}")
+  
+  def lt(self, other: 'Result', target, kernel: str = None) -> bool:
+    return self.get_value(target, kernel) < other.get_value(target, kernel)
+  
+  def eq(self, other: 'Result', target, kernel: str = None) -> bool:
+    return self.get_value(target, kernel) == other.get_value(target, kernel)
+  
+  def lte(self, other: 'Result', target, kernel: str = None) -> bool:
+    return self.get_value(target, kernel) <= other.get_value(target, kernel)
+  
+  def gt(self, other: 'Result', target, kernel: str = None) -> bool:
+    return self.get_value(target, kernel) > other.get_value(target, kernel)
+  
+  def gte(self, other: 'Result', target, kernel: str = None) -> bool:
+    return self.get_value(target, kernel) >= other.get_value(target, kernel)
+  
+  def __init__(self, output: str):
+    self.__parse_output(output)
+
+
+class Runner:
+  def __init__(self, exec: str, args: List[str]):
+    self.exec = exec
+    self.args = args
+    self.freq_results = {}
+
+  def run(self, freq: int = None) -> Result:
+    if freq in self.freq_results:
+      return self.freq_results[freq]
+    if freq:
+      set_frequency(freq)
+    proc = subprocess.run([self.exec, *self.args], capture_output=True, text=True)
+    res = Result(proc.stdout)
+    self.freq_results[freq] = res
+    return res
+  
+def get_most_relevant_kernels(freq: int, runner: 'Runner') -> List[str]:
+  res = runner.run(freq)
+  kernels = list(res.kernels.keys())
+  filtered_kernels = []
+  threshold = 10  
+  
+  while len(filtered_kernels) <= 1:
+    filtered_kernels = list(filter(lambda k: res.kernels[k].time_percentage_total >= threshold, kernels))
+    threshold /= 2
+  
+  return filtered_kernels
+  
+
+# binary search for the frequency
+def find_freq(target: str, kernel: str, frequencies: List[int], runner: Runner) -> int:
+  if len(frequencies) == 1:
+    return frequencies[0]
+  
+  l_freq = frequencies[0]
+  m_freq = frequencies[len(frequencies) // 2]
+  r_freq = frequencies[-1]
+  
+  l_res = runner.run(l_freq)
+  m_res = runner.run(m_freq)
+  r_res = runner.run(r_freq)
+  
+  print(f"{kernel}:  l={l_res.kernels[kernel].energy} ({l_freq}MHz) | m={m_res.kernels[kernel].energy} ({m_freq}MHz) | r={r_res.kernels[kernel].energy} ({r_freq}MHz)")
+  
+  if l_res.lt(m_res, target, kernel) and l_res.lt(r_res, target, kernel):
+    print("left-> ", end="")
+    l = 0
+    r = len(frequencies) // 2
+  elif m_res.lt(l_res, target, kernel) and m_res.lt(r_res, target, kernel):
+    print("mid-> ", end="")
+    l = len(frequencies) // 2 - len(frequencies) / 4
+    r = len(frequencies) // 2 + len(frequencies) / 4
+  else:
+    print("right-> ", end="")
+    l = len(frequencies) // 2
+    r = len(frequencies)
+    
+  l, r = int(l), int(r)
+  
+  print(f"l: {l} | r: {r}")
+  return find_freq(target, kernel, frequencies[l:r], runner)
+
+if __name__ == '__main__':
+  try:
+    parser = argparse.ArgumentParser(description="Find the frequency with the specified target")
+    # add enum parameter
+    parser.add_argument("--target", choices=["ME", "MP", "MEDP"], default="ME", help="The target to search for")
+    parser.add_argument("--runs", type=int, default=1, help="The number of runs to average")
+    parser.add_argument("arch", choices=["intel", "amd", "nvidia"], help="The architecture of the device")
+    parser.add_argument("exec", help="The executable to run")
+    parser.add_argument("args", nargs=argparse.REMAINDER, help="The arguments to pass to the executable")
+    
+    args = parser.parse_args()
+    
+    RUNS = args.runs
+    ARCH = args.arch
+    
+    args.target = Target.from_str(args.target)
+    args.exec = os.path.abspath(args.exec)
+    if not os.path.exists(args.exec):
+      print("The executable does not exist")
+      sys.exit(1)
+    
+    runner = Runner(args.exec, args.args)
+    most_relevant_kernels = get_most_relevant_kernels(get_default_freq(), runner)
+
+    frequencies = get_frequencies()
+    for kernel in most_relevant_kernels:
+      freq = find_freq(args.target, kernel, frequencies, runner)
+      print(f"Best frequency for optimizing {kernel} on target {args.target}: {freq}")
+      print(f"\t- Energy: {runner.run(freq).kernels[kernel].energy}")
+      print(f"\t- Time: {runner.run(freq).kernels[kernel].time}")
+      print(f"\t- MEDP: {runner.run(freq).kernels[kernel].energy * runner.run(freq).kernels[kernel].time}")
+    reset_frequency()
+  
+  except KeyboardInterrupt as e:
+    reset_frequency()
