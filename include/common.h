85d14c6ddacb59981cbd1142520a436f7d2aa263
antonio-decaro <antonio.decaro99@outlook.it>
2024-03-13 14:32:22 +0000

    inserted support for lulesh

--- include/common.h
+++ include/common.h
@@ -3,7 +3,7 @@
 
 #include <sycl/sycl.hpp>
 
-using namespace cl::sycl;
+using namespace sycl;
 constexpr access::mode sycl_read       = access::mode::read;
 constexpr access::mode sycl_write      = access::mode::write;
 constexpr access::mode sycl_read_write = access::mode::read_write;
